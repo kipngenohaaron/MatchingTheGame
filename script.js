1bc933ec7f4cf7fa9f983704f1ae96191372e4ee
kipngenohaaron <kipngenohaaron@gmail.com>
2023-05-22 12:51:09 +0300

    move and scores updated

--- script.js
+++ script.js
@@ -3,53 +3,32 @@ let firstSelection = null;
 let secondSelection = null;
 let isAnimating = false;
 let move = 0;
+let score = 0;
 
-
-const score = document.querySelector('.score span');
-
+const scoreDisplay = document.querySelector("#score-counter");
 const movesDisplay = document.querySelector("#moves-counter");
 const cardsContainer = document.querySelector(".cards");
-const scoreDisplay = document.querySelector("#score-counter");
-function updateScoreDisplay() {
-    scoreDisplay.textContent = score;
-  }
-
 const cards = document.querySelectorAll(".cards .card");
-// Function to update the moves counter display
+
+function updateScoreDisplay() {
+  scoreDisplay.textContent = score;
+}
+
 function updateMovesDisplay() {
   movesDisplay.textContent = move;
 }
+
 (function shuffle() {
-    cards.forEach(card => {
-      let randomPos = Math.floor(Math.random() *12);
-      card.style.order = randomPos;
-    });
-  })();
-// Shuffle the cards on page load
-// window.addEventListener("DOMContentLoaded", () => {
-//   shuffleCards();
-//   updateMovesDisplay();
-// });
-function match(cardOne , cardTwo){
-    if(cardOne.dataset.index == cardTwo.dataset.index){
-        score.innerHTML = parseInt(score.innerHTML) + 1
-        cardOne.classList.remove('flip')
-        cardTwo.classList.remove('flip')
-        cardOne.classList.add('match')
-        cardTwo.classList.add('match')
-    }else{
-        setTimeout(() => {
-            cardOne.classList.remove('flip')
-            cardTwo.classList.remove('flip')
-        }, 1000);
-    }
-}
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
 cards.forEach((card) => {
-
-
   card.addEventListener("click", () => {
     card.classList.add("clicked");
-   
+
     if (counter === 0) {
       firstSelection = card.getAttribute("fruit");
       counter++;
@@ -66,8 +45,9 @@ cards.forEach((card) => {
         correctCards[0].classList.remove("clicked");
         correctCards[1].classList.add("checked");
         correctCards[1].classList.remove("clicked");
-      } 
-      else {
+        score++; // Increment score for a match
+        updateScoreDisplay();
+      } else {
         const incorrectCards = document.querySelectorAll(".card.clicked");
 
         incorrectCards[0].classList.add("shake");
@@ -80,20 +60,11 @@ cards.forEach((card) => {
           incorrectCards[1].classList.remove("clicked");
         }, 800);
       }
+
+      move++;
+      updateMovesDisplay();
     }
   });
 });
-function match(cardOne , cardTwo){
-    if(cardOne.dataset.index == cardTwo.dataset.index){
-        score.innerHTML = parseInt(score.innerHTML) + 1
-        cardOne.classList.remove('flip')
-        cardTwo.classList.remove('flip')
-        cardOne.classList.add('match')
-        cardTwo.classList.add('match')
-    }else{
-        setTimeout(() => {
-            cardOne.classList.remove('flip')
-            cardTwo.classList.remove('flip')
-        }, 1000);
-    }
-}
+
+  
